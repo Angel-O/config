59dcf928dc9f1be11a7c2a0d392d1d85e6a83623
Angelo Oparah <16281580+Angel-O@users.noreply.github.com>
2026-03-07 19:54:17 +0000

    Make gwtco cd into the created worktree

--- worktree-functions.zsh
+++ worktree-functions.zsh
@@ -1,9 +1,23 @@
-# Oh My Zsh-style shorthand wrappers around the git worktree aliases defined in
-# the gitconfig template.
+# Oh My Zsh-style wrappers around the git worktree aliases defined in the
+# gitconfig template.
 alias gwtl='git wtl'
 alias gwtp='git wtp'
-alias gwtco='git wtco'
 alias gwtrm='git wtrm'
+
+# Create or attach the requested worktree via `git wtco`, then cd into it so
+# the command feels closer to `gco` in day-to-day use.
+gwtco() {
+  local branch
+
+  if [ "$1" = "-b" ]; then
+    branch="$2"
+  else
+    branch="$1"
+  fi
+
+  git wtco "$@" || return
+  cdwt "$branch" || return
+}
 
 # cd into the directory associated to the given worktree. Assumes the
 # worktree lives in a sibling directory suffixed with `-worktrees`.
